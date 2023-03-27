cbba469963a4c6e6fac8524ca650ed48117a26bf
OllieJC <5426038+OllieJC@users.noreply.github.com>
2023-03-27 14:30:56 +0100

    Add zendesk backup lambda

---
+++ lambda/zendesk-backup/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+rm ./*.zip || echo "No ZIPs to delete"
+rm -rf .target || echo "No .target/ to delete"
+mkdir .target
+
+python3.9 -m pip install -r requirements.txt -t .target/ --upgrade --no-user
+
+cp ./*.py .target/
+
+cd .target/ || exit 1
+
+find . -type f -exec chmod 0644 {} \;
+find . -type d -exec chmod 0755 {} \;
+
+zip -r ../target.zip .
+
+cd ../
