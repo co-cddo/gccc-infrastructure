eb6addf4c8d3464fd97c15af1845c4a0c4fbbd5c
OllieJC <5426038+OllieJC@users.noreply.github.com>
2023-08-10 10:53:18 +0100

    Update main.py

--- lambda/hackerone-zendesk-integration/main.py
+++ lambda/hackerone-zendesk-integration/main.py
@@ -7,23 +7,29 @@ import hackerone
 def lambda_handler(event, context):
     if "report_id" in event:
         hackerone_report = hackerone.get_hackerone_report(report_id=event["report_id"])
-        if hackerone_report and hackerone_report.get("triaged_at", None) is not None:
-            zid = zendesk.create_or_update_zendesk_ticket(hackerone_report)
-
-            print(
-                json.dumps(
-                    {"hackerone_report": hackerone_report, "zendesk_id": zid},
-                    default=str,
-                )
-            )
-
-            if hackerone_report["issue_tracker_reference_id"]:
+        if hackerone_report:
+            if hackerone_report.get("triaged_at", None) is None:
                 print(
-                    "Found existing reference_id in HackerOne:",
-                    hackerone_report["issue_tracker_reference_id"],
+                    "HackerOne report has not yet been triaged:",
+                    event["report_id"],
                 )
             else:
-                print("Setting reference_id in HackerOne:", zid)
-                hackerone.set_hackerone_reference(
-                    hackerone_id=event["report_id"], zendesk_id=zid
+                zid = zendesk.create_or_update_zendesk_ticket(hackerone_report)
+
+                print(
+                    json.dumps(
+                        {"hackerone_report": hackerone_report, "zendesk_id": zid},
+                        default=str,
+                    )
                 )
+
+                if hackerone_report["issue_tracker_reference_id"]:
+                    print(
+                        "Found existing reference_id in HackerOne:",
+                        hackerone_report["issue_tracker_reference_id"],
+                    )
+                else:
+                    print("Setting reference_id in HackerOne:", zid)
+                    hackerone.set_hackerone_reference(
+                        hackerone_id=event["report_id"], zendesk_id=zid
+                    )
